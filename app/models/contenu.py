b8d238f03b4a38d66205d1c0d86688423b64b571
andreas2104 <andrianiainaandreas99@gmail.com>
2025-07-25 00:46:56 +0300

    fix deleting key

--- app/models/contenu.py
+++ app/models/contenu.py
@@ -13,14 +13,14 @@ class Contenu(db.Model):
   __tablename__ = "contenu"
 
   id_contenu = db.Column(db.Integer, primary_key=True)
-  id_utilisateur = db.Column(db.Integer, db.foreignkey('utilisateur_id'))
-  id_modele = db.Column(db.Integer, db.foreignKey('model_id'))
-  titre = db.Column(db.String(255), nullable=False)
-  type_contenu = db.Column(db.ENum(TypeContenuEnum), default=TypeContenuEnum.text, nullable=False)
+  id_utilisateur = db.Column(db.Integer, db.ForeignKey('utilisateurs.id'))
+  id_modele = db.Column(db.Integer, db.ForeignKey('model_ia.id'))
+  titre = db.Column(db.String(255), nullable=True)
+  type_contenu = db.Column(db.Enum(TypeContenuEnum), default=TypeContenuEnum.text, nullable=False)
   texte = db.Column(db.String, nullable=True)
   image_url = db.Column(db.String(300), nullable=True)
   meta = db.Column(db.JSON, nullable= True)
-  date_creation = db.Column(db.dateTime, default=datetime.utcnow)
+  date_creation = db.Column(db.DateTime, default=datetime.utcnow)
 
 def __repr__(self):
   return f"<Prompt {self.id}: {self.nom_prompt}"

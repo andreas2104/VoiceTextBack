99bfa2fcc3509f4b267ea215427ffa24208a0da9
andreas2104 <andrianiainaandreas99@gmail.com>
2025-09-19 20:45:11 +0300

    fix token issue

--- app/routes/template_routes.py
+++ app/routes/template_routes.py
@@ -1,24 +1,31 @@
 from flask import Blueprint
 from app.controllers import template_controller
+from flask_jwt_extended import jwt_required
+
 
 template_bp = Blueprint('template_bp', __name__)
 
 @template_bp.route("/", methods=['GET'])
+@jwt_required()
 def get_all_template():
   return template_controller.get_all_template()
 
 @template_bp.route("/",methods=['POST'])
+@jwt_required()
 def create_template():
   return template_controller.create_template()
 
 @template_bp.route("/<int:template_id>", methods=['GET'])
+@jwt_required()
 def get_template_by_id(template_id):
   return template_controller.get_template_by_id(template_id)
 
 @template_bp.route("/<int:template_id>", methods=["PUT"])
+@jwt_required()
 def update_template(template_id):
   return template_controller.update_template(template_id)
 
 @template_bp.route("/<int:template_id>", methods=['DELETE'])
+@jwt_required()
 def delete_template(template_id):
   return template_controller.delete_template(template_id)
--- app/routes/utilisateur_routes.py
+++ app/routes/utilisateur_routes.py
@@ -1,9 +1,11 @@
 from flask import Blueprint
 from app.controllers import utilisateur_controller
+from flask_jwt_extended import jwt_required, get_jwt_identity
 
 utilisateur_bp = Blueprint('utilisateur_bp', __name__)
 
 @utilisateur_bp.route("/", methods=["GET"])
+@jwt_required()
 def get_all_utilisateur():
     return utilisateur_controller.get_all_utilisateurs()
 
@@ -12,20 +14,23 @@ def create_utilisateur():
     return utilisateur_controller.create_utilisateur()  
 
 @utilisateur_bp.route("/<int:utilisateur_id>", methods=["GET"])
+@jwt_required()
 def get_utilisateur_by_id(utilisateur_id):
     return utilisateur_controller.get_utilisateur_by_id(utilisateur_id)
 
 @utilisateur_bp.route("/<int:utilisateur_id>", methods=["PUT"])
+@jwt_required()
 def update_utilisateur(utilisateur_id):
     return utilisateur_controller.update_utilisateur(utilisateur_id)
 
 @utilisateur_bp.route("/<int:utilisateur_id>", methods=["DELETE"])
+@jwt_required()
 def delete_utilisateur(utilisateur_id):
     return utilisateur_controller.delete_utilisateur(utilisateur_id)
 
 from app.utils.roles import roles_required
-
 @utilisateur_bp.route("/me", methods=["GET"])
+@jwt_required()
 def current_utilisateur():
     return utilisateur_controller.current_utilisateur()
 
